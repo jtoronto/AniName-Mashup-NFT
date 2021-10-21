64fa345eba5f18cbd2300e326f3eb4ed548416be
jtoronto <joseph.toronto@yahoo.com>
2021-10-21 11:25:25 -0400

    Initial Commit

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,19 +1,19 @@
 const main = async () => {
-  const nftContractFactory = await hre.ethers.getContractFactory("PerfectCats");
+  const nftContractFactory = await hre.ethers.getContractFactory("MyEpicNFT");
   const nftContract = await nftContractFactory.deploy();
   await nftContract.deployed();
   console.log("Contract deployed to:", nftContract.address);
 
   // Call the function.
-  let txn = await nftContract.SpawnCat();
+  let txn = await nftContract.makeAnEpicNFT();
   // Wait for it to be mined.
   await txn.wait();
   console.log("Minted NFT #1");
 
-  txn = await nftContract.SpawnCat();
-  // Wait for it to be mined.
-  await txn.wait();
-  console.log("Minted NFT #2");
+  //   txn = await nftContract.makeAnEpicNFT();
+  //   // Wait for it to be mined.
+  //   await txn.wait();
+  //   console.log("Minted NFT #2");
 };
 
 const runMain = async () => {
--- scripts/run.js
+++ scripts/run.js
@@ -4,15 +4,21 @@ const main = async () => {
   await nftContract.deployed();
   console.log("Contract deployed to:", nftContract.address);
 
-  // Call the function.
-  let txn = await nftContract.makeAnEpicNFT();
-  // Wait for it to be mined.
-  await txn.wait();
+  for (i = 0; i < 10; i++) {
+    // Call the function.
+    let txn = await nftContract.makeAnEpicNFT();
+    // Wait for it to be mined.
+    await txn.wait();
+  }
 
-  // Mint another NFT for fun.
-  txn = await nftContract.makeAnEpicNFT();
-  // Wait for it to be mined.
-  await txn.wait();
+  //   // Mint another NFT for fun.
+  //   txn = await nftContract.makeAnEpicNFT();
+  //   // Wait for it to be mined.
+  //   await txn.wait();
+
+  //   txn = await nftContract.makeAnEpicNFT();
+  //   // Wait for it to be mined.
+  //   await txn.wait();
 };
 
 const runMain = async () => {
