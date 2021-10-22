4f9ce7599b235fa511ebfc6766d2656862261f69
jtoronto <joseph.toronto@yahoo.com>
2021-10-22 12:35:17 -0400

    Class project complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,27 @@
 import "./styles/App.css";
 import twitterLogo from "./assets/twitter-logo.svg";
+import loadingSpinner from "./assets/loading-waiting.gif";
 import myEpicNft from "./utils/MyEpicNFT.json";
 import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
 
-const TWITTER_HANDLE = "_buildspace";
+const TWITTER_HANDLE = "jtoronto34";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
-const OPENSEA_LINK = "";
+const OPENSEA_LINK = "https://testnets.opensea.io/collection/squarenft-t1q6qkrwm1";
 var TOTAL_MINT_COUNT = 0; //Literally only using these for console.log right now since state doesn't update immediately,
 var MAX_COUNT = 0
 
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
+  const [connectedToWrongNetwork, setConnectedToWrongNetwork] = useState(true);
   const [totalMintCount, setTotalMintCount] = useState(0);
   const [maxTokens, setMaxTokens] = useState(0);
-  const CONTRACT_ADDRESS = "0x4315c4cEc4e1A95267489dDF05cF3A2DCCFD60e0"; //Ganache local
-  //const CONTRACT_ADDRESS = "0xBB87Fe930703Ff853b63A607c5e6e34f88eF0629"; //Rinkeby local
+  const [currentlyMining, setCurrentlyMining] = useState(false);
+
+  const [randomCatImage, setRandomCatImage] = useState();
+  //const CONTRACT_ADDRESS = "0x4315c4cEc4e1A95267489dDF05cF3A2DCCFD60e0"; //Ganache local
+  const CONTRACT_ADDRESS = "0x1267537789277E8f9C0c9F762d1C305a05136275"; //Rinkeby local
 
   const checkIfWalletIsConnected = async () => {
     const { ethereum } = window;
@@ -34,6 +39,15 @@ const App = () => {
       const account = accounts[0];
       console.log("Found an authorized account:", account);
       setCurrentAccount(accounts[0]);
+
+      let chainId = await ethereum.request({ method: 'eth_chainId' });
+      console.log("Connected to chain " + chainId);
+      // String, hex code of the chainId of the Rinkebey test network
+      const rinkebyChainId = "0x4"; 
+      if (chainId === rinkebyChainId) {
+        setConnectedToWrongNetwork(false);
+      }
+      
       //setupEventListener();
     } else {
       console.log("No authorized account found");
@@ -66,18 +80,41 @@ const App = () => {
       setCurrentAccount(accounts[0]);
       // Setup listener! This is for the case where a user comes to our site
       // and connected their wallet for the first time.
-      setupEventListener();
     } catch (error) {
       console.log(error);
     }
   };
   // Setup our listener.
-  const setupEventListener = async () => {
+  const setupWalletEventListener = async () =>{
+
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        //Set up wallet chain listener  
+        ethereum.on('chainChanged', (_chainId) => {
+          console.log('user chain changed to', _chainId);
+          window.location.reload()}
+        );
+        //set up account chain listener
+        ethereum.on('accountsChanged', (_accounts) => window.location.reload());
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
+  const setupNFTMinedEventListener = async () => {
     // Most of this looks the same as our function askContractToMintNft
     try {
       const { ethereum } = window;
 
       if (ethereum) {
+       
         // Same stuff again
         const provider = new ethers.providers.Web3Provider(ethereum);
         const signer = provider.getSigner();
@@ -90,11 +127,13 @@ const App = () => {
         // THIS IS THE MAGIC SAUCE.
         // This will essentially "capture" our event when our contract throws it.
         // If you're familiar with webhooks, it's very similar to that!
+        
         connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
           console.log(from, tokenId.toNumber());
           alert(
             `Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`
           );
+          getTotalNFTsMintedSoFar();
         });
 
         console.log("Setup event listener!");
@@ -119,15 +158,19 @@ const App = () => {
           provider
         );
 
-        
-        TOTAL_MINT_COUNT = (await connectedContract.getTotalMintCount()).toNumber();
-        setTotalMintCount((await connectedContract.getTotalMintCount()).toNumber());
+       TOTAL_MINT_COUNT = (await connectedContract.getTotalMintCount()).toNumber();
+        setTotalMintCount(TOTAL_MINT_COUNT);
+        console.log("Total mint count:", TOTAL_MINT_COUNT);
 
         MAX_COUNT = (await connectedContract.maxTokens()).toNumber();
-        setMaxTokens((await connectedContract.maxTokens()).toNumber());
-        console.log("Total mint count:", TOTAL_MINT_COUNT);
+        setMaxTokens(MAX_COUNT);
         console.log("Max allowed tokens", MAX_COUNT);
+
       } else {
+        //MAX_COUNT = (await connectedContract.maxTokens()).toNumber();
+        
+        
+        
         console.log("Ethereum object doesn't exist!");
       }
     } catch (error) {
@@ -148,17 +191,19 @@ const App = () => {
           signer
         );
 
-        if (totalMintCount => maxTokens){
+        if (TOTAL_MINT_COUNT >= MAX_COUNT){
           alert("Sorry but the maximum amount of tokens has already been minted");
           return;
         }
 
         console.log("Going to pop wallet now to pay gas...");
         let nftTxn = await connectedContract.makeAnEpicNFT();
+        setupNFTMinedEventListener();
 
         console.log("Mining...please wait.");
+        setCurrentlyMining(true);
         await nftTxn.wait();
-
+        setCurrentlyMining(false);
         console.log(
           `Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`
         );
@@ -172,6 +217,7 @@ const App = () => {
 
   useEffect(() => {
     checkIfWalletIsConnected();
+    setupWalletEventListener();
     getTotalNFTsMintedSoFar();
   }, []);
 
@@ -182,6 +228,15 @@ const App = () => {
     <div className="App">
       <div className="container">
         <div className="header-container">
+          {
+             connectedToWrongNetwork && (
+              <div className="connectedIndicator">
+                You aren't connected to the rinkeby network
+              </div>
+             )
+
+          }
+          
           <p className="header gradient-text">My NFT Collection</p>
           <p className="sub-text">
             Each unique. Each beautiful. Discover your NFT today.
@@ -202,15 +257,34 @@ const App = () => {
             </button>
           )}
           <div style={{color: 'white', paddingTop: '20px'}}>{totalMintCount} of {maxTokens} tokens minted!</div>
+          {
+            currentlyMining &&(<div className="miningIndicator">
+            <div style={{paddingBottom: '30px'}}>Mining, please wait...</div>
+            <div><img src={loadingSpinner} height="100" width="100" /></div>
+
+          </div>)
+          }
+           
         </div>
         <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-          <a
-            className="footer-text"
-            href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+        
+        <a href={OPENSEA_LINK} 
+          target="_blank"
+          rel="noopener noreferrer">
+            <button
+            style={{bottom:'20px'}}
+              className="cta-button connect-wallet-button">
+                  See the collection on OpenSea  
+                   </button>  </a>
+          <div style={{display:"flex",
+                      paddingTop:'50px'}}>
+                          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+                            <a
+                              className="footer-text"
+                              href={TWITTER_LINK}
+                              target="_blank"
+                              rel="noreferrer"
+                            >{`built by @${TWITTER_HANDLE}`}</a></div>
         </div>
       </div>
     </div>
